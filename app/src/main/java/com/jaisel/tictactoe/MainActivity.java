0f2068ebbfced6ea50f2e2a682a6a8aa2cab997f
JaiselRahman <jaisel20@gmail.com>
2017-01-26 14:17:29 +0530

    Added Select XO dialog and ttt class

--- app/src/main/java/com/jaisel/tictactoe/MainActivity.java
+++ app/src/main/java/com/jaisel/tictactoe/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import android.graphics.*;
 
 public class MainActivity extends Activity 
 {
@@ -18,11 +19,89 @@ public class MainActivity extends Activity
 			public void onClick(View v)
 			{
 				Intent i=new Intent(getApplicationContext(),xo.class);
-				startActivityForResult(i,RESULT_OK);
-				//finish();
+				startActivityForResult(i,0);
 			}
-		});
-		
-	
+		});	
     }
 }
+class ttt
+{
+	char board[]={'-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+	char computerLetter, playerLetter;
+	boolean isWinner(char tboard[], char l)
+	{
+		return ((tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
+				(tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
+				(tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
+				(tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
+				(tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
+				(tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
+				(tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
+				(tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
+				(tboard[1] == l && tboard[2] == l && tboard[3] == l));
+	}
+	boolean isFree(int pos)
+	{
+		return board[pos] == ' ';
+	}
+	boolean isFull()
+	{
+		for(int i=1;i<10;i++)
+		{
+			if(board[i]!=' ')
+				return false;
+		}
+		return true;
+	}
+	char[] getBoardCopy()
+	{
+		char t[]=new char[board.length];
+		for(int i=0;i<board.length;i++)
+			t[i]=board[i];
+		return t;
+	}
+	void makeMove(char _board[], int pos, char l)
+	{
+		_board[pos]=l;
+	}
+	int randomMove(int move[])
+	{
+		int possibleMove[]=new int[move.length];
+		int n=0;
+		for(int i=0; i<move.length; i++)
+		{
+			if(isFree(move[i]))
+			{
+				possibleMove[n]=move[i];
+				n++;
+			}
+		}
+		if(n==0)
+			return 0;
+		return possibleMove[((int)Math.random()*10) % n];
+	}
+	int compMove()
+	{
+		int pos=0;
+		char[] t;	
+		for(int i=0; i< 10; i++)
+			if(isFree(i))
+			{
+				t=getBoardCopy();
+				makeMove(t, i, computerLetter);
+				if(isWinner(t, computerLetter))
+					return i;
+			}
+		for(int i=0; i< 10; i++)
+			if(isFree(i))
+			{
+				t=getBoardCopy();
+				makeMove(t, i, playerLetter);
+				if(isWinner(t, playerLetter))
+					return i;
+			}
+		return pos;
+	}
+}
+
+
--- app/src/main/java/com/jaisel/tictactoe/xo.java
+++ app/src/main/java/com/jaisel/tictactoe/xo.java
@@ -3,37 +3,88 @@ package com.jaisel.tictactoe;
 import android.app.*;
 import android.content.*;
 import android.os.*;
+import android.view.*;
 
-public class xo extends Activity
+public class xo extends Activity implements View.OnClickListener
 {
+	AlertDialog.Builder gameExit, chooseXO;
+	ttt t;
 	@Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.xoboard);
-	}
-	@Override
-	public void onBackPressed() {
-		DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+		t = new ttt();
+		DialogInterface.OnClickListener gameExitListener = new DialogInterface.OnClickListener()
+		{
 			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				switch (which){
+			public void onClick(DialogInterface dialog, int which)
+			{
+				switch (which)
+				{
 					case DialogInterface.BUTTON_POSITIVE:
 						finish();
-						
 						break;
-
 					case DialogInterface.BUTTON_NEGATIVE:
-						//No button clicked
 						break;
 				}
 			}
 		};
+		gameExit = new AlertDialog.Builder(xo.this);
+		gameExit.setMessage("Are you sure?\nThe current game will be lost.")
+			.setPositiveButton("Yes", gameExitListener)
+			.setNegativeButton("No", gameExitListener);
+		DialogInterface.OnClickListener chooseXOListener = new DialogInterface.OnClickListener()
+		{
+			@Override
+			public void onClick(DialogInterface dialog, int which)
+			{
+				switch (which)
+				{
+					case DialogInterface.BUTTON_POSITIVE:
+						t.playerLetter='O';
+						break;
+					case DialogInterface.BUTTON_NEGATIVE:
+						t.computerLetter='X';
+						break;
+				}
+			}
+		};
+		chooseXO = new AlertDialog.Builder(xo.this);
+		chooseXO.setMessage("Select X or O")
+			.setPositiveButton("O", chooseXOListener)
+			.setNegativeButton("X", chooseXOListener);
+		chooseXO.show();
+	}
+	@Override
+	public void onBackPressed()
+	{
+			gameExit.show();
+	}
 
-		AlertDialog.Builder builder = new AlertDialog.Builder(xo.this);
-		builder.setMessage("Are you sure?\nThe current game will be lost.")
-			.setPositiveButton("Yes", dialogClickListener)
-			.setNegativeButton("No", dialogClickListener)
-			.show();
+	@Override
+	public void onClick(View p1)
+	{
+		switch(p1.getId())
+		{
+			case R.id.TL:
+				break;
+			case R.id.TM:
+				break;
+			case R.id.TR:
+				break;
+			case R.id.ML:
+				break;
+			case R.id.MM:
+				break;
+			case R.id.MR:
+				break;
+			case R.id.BL:
+				break;
+			case R.id.BM:
+				break;
+			case R.id.BR:
+				break;
+		}
 	}
 }
