825186b201c427388af32ae0bd2284b8f1649e70
JaiselRahman <jaisel20@gmail.com>
2017-01-29 09:56:20 +0530

    Added Play against Computer

--- app/src/main/java/com/jaisel/tictactoe/MainActivity.java
+++ app/src/main/java/com/jaisel/tictactoe/MainActivity.java
@@ -9,36 +9,74 @@ import android.graphics.*;
 
 public class MainActivity extends Activity 
 {
+	private Menu menu;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-		Button Player = (Button)findViewById(R.id.player);
-		Player.setOnClickListener(new View.OnClickListener(){
-			public void onClick(View v)
+		Button Computer = (Button)findViewById(R.id.computer);
+		Computer.setOnClickListener(new View.OnClickListener()
 			{
-				Intent i=new Intent(getApplicationContext(),xo.class);
-				startActivityForResult(i,0);
-			}
-		});	
+				public void onClick(View v)
+				{
+					Intent i=new Intent(getApplicationContext(), xo.class);
+					startActivityForResult(i, 0);
+				}
+			});	
     }
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu)
+	{
+		this.menu=menu;
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+	private boolean inmenu=false;
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+        switch (item.getItemId())
+		{
+			case R.id.about:
+				setContentView(R.layout.about);
+				menu.setGroupVisible(R.id.menu_group,false);
+				inmenu = true;
+				return true;
+			case R.id.exit:
+				System.exit(0);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+        } 
+	}
+	@Override
+	public void onBackPressed()
+	{
+		if (inmenu)
+		{
+			this.recreate();
+			menu.setGroupVisible(R.id.menu_group,true);
+			inmenu = false;
+		}
+		else
+			super.onBackPressed();
+	}
 }
 class ttt
 {
 	char board[]={'-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-	char computerLetter, playerLetter;
+	char computerLetter='O', playerLetter='X';
+	char first='P',second='C';
 	boolean isWinner(char tboard[], char l)
 	{
 		return ((tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
-				(tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
-				(tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
-				(tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
-				(tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
-				(tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
-				(tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
-				(tboard[1] == l && tboard[2] == l && tboard[3] == l) ||
-				(tboard[1] == l && tboard[2] == l && tboard[3] == l));
+			(tboard[4] == l && tboard[5] == l && tboard[6] == l) ||
+			(tboard[7] == l && tboard[8] == l && tboard[9] == l) ||
+			(tboard[1] == l && tboard[4] == l && tboard[7] == l) ||
+			(tboard[2] == l && tboard[5] == l && tboard[8] == l) ||
+			(tboard[3] == l && tboard[6] == l && tboard[9] == l) ||
+			(tboard[1] == l && tboard[5] == l && tboard[9] == l) ||
+			(tboard[3] == l && tboard[5] == l && tboard[7] == l));
 	}
 	boolean isFree(int pos)
 	{
@@ -46,9 +84,9 @@ class ttt
 	}
 	boolean isFull()
 	{
-		for(int i=1;i<10;i++)
+		for (int i=1;i < 10;i++)
 		{
-			if(board[i]!=' ')
+			if (board[i] == ' ')
 				return false;
 		}
 		return true;
@@ -56,51 +94,81 @@ class ttt
 	char[] getBoardCopy()
 	{
 		char t[]=new char[board.length];
-		for(int i=0;i<board.length;i++)
-			t[i]=board[i];
+		for (int i=0;i < board.length;i++)
+			t[i] = board[i];
 		return t;
 	}
 	void makeMove(char _board[], int pos, char l)
 	{
-		_board[pos]=l;
+		_board[pos] = l;
 	}
 	int randomMove(int move[])
 	{
 		int possibleMove[]=new int[move.length];
 		int n=0;
-		for(int i=0; i<move.length; i++)
+		for (int i=0; i < move.length; i++)
 		{
-			if(isFree(move[i]))
+			if (isFree(move[i]))
 			{
-				possibleMove[n]=move[i];
+				possibleMove[n] = move[i];
 				n++;
 			}
 		}
-		if(n==0)
+		if (n == 0)
 			return 0;
-		return possibleMove[((int)Math.random()*10) % n];
+		return possibleMove[(int)(Math.random() * 10) % n];
+	}
+	char chooseFirst()
+	{
+		if ((int)(Math.random() * 10) % 2 == 0)
+		{
+			first = 'P';
+			second = 'C';
+			return 'P';
+		}
+		else 
+		{
+			first = 'C';
+			second = 'P';
+			return 'C';
+		}
 	}
 	int compMove()
 	{
-		int pos=0;
-		char[] t;	
-		for(int i=0; i< 10; i++)
-			if(isFree(i))
+		char[] t;
+		int c1[],c2[];
+		for (int i=1; i < 10; i++)
+			if (isFree(i))
 			{
-				t=getBoardCopy();
+				t = getBoardCopy();
 				makeMove(t, i, computerLetter);
-				if(isWinner(t, computerLetter))
+				if (isWinner(t, computerLetter))
 					return i;
 			}
-		for(int i=0; i< 10; i++)
-			if(isFree(i))
+		for (int i=1; i < 10; i++)
+			if (isFree(i))
 			{
-				t=getBoardCopy();
+				t = getBoardCopy();
 				makeMove(t, i, playerLetter);
-				if(isWinner(t, playerLetter))
+				if (isWinner(t, playerLetter))
 					return i;
 			}
-		return pos;
+		if (first == 'P')
+		{
+			c1 = new int[] {2, 4, 6,8};
+			c2 = new int[] {1, 3, 7, 9};
+		}
+		else
+		{
+			c1 = new int[] {1, 3, 7, 9};
+			c2 = new int[] {2, 4, 6, 9};
+		}
+		int pos=randomMove(c1);
+		if (pos != 0)
+			return pos;
+		if (isFree(5))
+			return 5;
+		return randomMove(c2);
 	}
 }
 
--- app/src/main/java/com/jaisel/tictactoe/xo.java
+++ app/src/main/java/com/jaisel/tictactoe/xo.java
@@ -2,19 +2,95 @@ package com.jaisel.tictactoe;
 
 import android.app.*;
 import android.content.*;
+import android.content.res.*;
 import android.os.*;
 import android.view.*;
+import android.widget.*;
 
-public class xo extends Activity implements View.OnClickListener
+
+public class xo extends Activity implements Button.OnClickListener
 {
-	AlertDialog.Builder gameExit, chooseXO;
+	AlertDialog.Builder chooseXO;
+	DialogInterface.OnClickListener gameFinished;
 	ttt t;
+	char player='P';
+	boolean isPlaying=false;
+	Button board[];
+	Button reset,confirm;
+	int pos=0;
 	@Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.xoboard);
 		t = new ttt();
+		board = new Button[10];
+		board[1] = (Button)findViewById(R.id.TL);
+		board[2] = (Button)findViewById(R.id.TM);
+		board[3] = (Button)findViewById(R.id.TR);
+		board[4] = (Button)findViewById(R.id.ML);
+		board[5] = (Button)findViewById(R.id.MM);
+		board[6] = (Button)findViewById(R.id.MR);
+		board[7] = (Button)findViewById(R.id.BL);
+		board[8] = (Button)findViewById(R.id.BM);
+		board[9] = (Button)findViewById(R.id.BR);
+		for (int i=1;i < 10;i++)
+		{
+			board[i].setOnClickListener(this);
+		}
+		reset = (Button)findViewById(R.id.reset);
+		reset.setOnClickListener(this);
+		confirm = (Button)findViewById(R.id.confirm);
+		confirm.setOnClickListener(this);
+		DialogInterface.OnClickListener chooseXOListener = new DialogInterface.OnClickListener()
+		{
+			@Override
+			public void onClick(DialogInterface dialog, int which)
+			{
+				switch (which)
+				{
+					case DialogInterface.BUTTON_POSITIVE:
+						t.playerLetter = 'O';
+						t.computerLetter = 'X';
+						break;
+					case DialogInterface.BUTTON_NEGATIVE:
+						t.playerLetter = 'X';
+						t.computerLetter = 'O';
+						break;
+					case DialogInterface.BUTTON_NEUTRAL:
+						finish();
+						return;
+				}
+				player = t.chooseFirst();
+				if (player == 'C')
+				{
+					Toast.makeText(xo.this.getApplicationContext(), "Computer goes first !", Toast.LENGTH_SHORT).show();
+					playComputer();
+				}
+				else
+					Toast.makeText(xo.this.getApplicationContext(), "You goes first !", Toast.LENGTH_SHORT).show();
+
+			}
+		};
+		chooseXO = new AlertDialog.Builder(xo.this);
+		chooseXO.setMessage("Select X or O")
+			.setPositiveButton("O", chooseXOListener)
+			.setNegativeButton("X", chooseXOListener)
+			.setNeutralButton("Exit", chooseXOListener)
+			.setCancelable(false)
+			.show();
+		gameFinished = new DialogInterface.OnClickListener()
+		{
+			@Override
+			public void onClick(DialogInterface dialog, int which)
+			{
+				recreate();
+			}
+		};
+	}
+	@Override
+	public void onBackPressed()
+	{
 		DialogInterface.OnClickListener gameExitListener = new DialogInterface.OnClickListener()
 		{
 			@Override
@@ -30,61 +106,102 @@ public class xo extends Activity implements View.OnClickListener
 				}
 			}
 		};
-		gameExit = new AlertDialog.Builder(xo.this);
+		AlertDialog.Builder gameExit = new AlertDialog.Builder(xo.this);
 		gameExit.setMessage("Are you sure?\nThe current game will be lost.")
 			.setPositiveButton("Yes", gameExitListener)
 			.setNegativeButton("No", gameExitListener);
-		DialogInterface.OnClickListener chooseXOListener = new DialogInterface.OnClickListener()
-		{
-			@Override
-			public void onClick(DialogInterface dialog, int which)
-			{
-				switch (which)
-				{
-					case DialogInterface.BUTTON_POSITIVE:
-						t.playerLetter='O';
-						break;
-					case DialogInterface.BUTTON_NEGATIVE:
-						t.computerLetter='X';
-						break;
-				}
-			}
-		};
-		chooseXO = new AlertDialog.Builder(xo.this);
-		chooseXO.setMessage("Select X or O")
-			.setPositiveButton("O", chooseXOListener)
-			.setNegativeButton("X", chooseXOListener);
-		chooseXO.show();
-	}
-	@Override
-	public void onBackPressed()
-	{
+		if (isPlaying)
 			gameExit.show();
+		else
+			finish();			
 	}
+	void playComputer()
+	{
+		int move=t.compMove();
+		t.makeMove(t.board, move, t.computerLetter);
+		if (move != 0) board[move].setText("" + t.computerLetter + "");
+		if (t.isWinner(t.board, t.computerLetter))
+		{
+			new AlertDialog.Builder(xo.this)
+				.setMessage("You Computer Has Beaten You!\nYou lose.")
+				.setPositiveButton("Ok", gameFinished)
+				.show();
+		}
+		else
+		if (t.isFull())
+		{
+			new AlertDialog.Builder(xo.this)
+				.setMessage("The Game Is A Tie")
+				.setPositiveButton("Ok", gameFinished)
+				.show();				
+		}
+		else
+			player = 'P';
+	}
+	void toggleText(int _pos)
+	{
 
+		if (board[_pos].getText().charAt(0) == '-')
+		{
+			board[_pos].setText("" + t.playerLetter + "");
+		}
+		else
+		{
+			board[_pos].setText("-");
+		}
+		for (int i=1;i < 10;i++)
+		{
+			if (t.isFree(i) && i != _pos)
+				board[i].setText("-");
+		}
+	}
 	@Override
 	public void onClick(View p1)
 	{
-		switch(p1.getId())
+		isPlaying = true;
+		if (player == 'P')
 		{
-			case R.id.TL:
-				break;
-			case R.id.TM:
-				break;
-			case R.id.TR:
-				break;
-			case R.id.ML:
-				break;
-			case R.id.MM:
-				break;
-			case R.id.MR:
-				break;
-			case R.id.BL:
-				break;
-			case R.id.BM:
-				break;
-			case R.id.BR:
-				break;
+			switch (p1.getId())
+			{
+				case R.id.TL: pos = 1; if (t.isFree(pos)) toggleText(pos); break;
+				case R.id.TM: pos = 2; if (t.isFree(pos)) toggleText(pos); break;
+				case R.id.TR: pos = 3; if (t.isFree(pos)) toggleText(pos); break;
+				case R.id.ML: pos = 4; if (t.isFree(pos)) toggleText(pos); break;
+				case R.id.MM: pos = 5; if (t.isFree(pos)) toggleText(pos); break;
+				case R.id.MR: pos = 6; if (t.isFree(pos)) toggleText(pos); break;
+				case R.id.BL: pos = 7; if (t.isFree(pos)) toggleText(pos); break;
+				case R.id.BM: pos = 8; if (t.isFree(pos)) toggleText(pos); break;
+				case R.id.BR: pos = 9; if (t.isFree(pos)) toggleText(pos); break;
+				case R.id.reset: this.recreate(); break;
+				case R.id.confirm: 
+					if (t.isFree(pos))
+					{
+						t.makeMove(t.board, pos, t.playerLetter);
+						board[pos].setText("" + t.playerLetter + "");
+						if (t.isWinner(t.board, t.playerLetter))
+						{
+							new AlertDialog.Builder(xo.this)
+								.setMessage("You Won The Game !")
+								.setPositiveButton("Ok", gameFinished)
+								.show();
+						}
+						else
+						if (t.isFull())
+						{
+							new AlertDialog.Builder(xo.this)
+								.setMessage("The Game Is A Tie")
+								.setPositiveButton("Ok", gameFinished)
+								.show();
+						}
+						else
+							player = 'C';
+					}
+					break;
+			}
+		}
+		if (player == 'C')
+		{
+			playComputer();
 		}
 	}
 }
