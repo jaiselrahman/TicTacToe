aff99d6f747c16575515231f3f84bebd9e11241c
Jaisel Rahman <jaiselrahman@gmail.com>
2019-06-04 23:55:33 +0530

    Update firebase functions to send play request

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -2,8 +2,6 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-const db = admin.firestore();
-
 exports.updateContacts = functions.https.onRequest((request, response) => {
   if (request.method !== "POST") {
     response.send(405, 'HTTP Method ' + request.method + ' not allowed');
@@ -16,7 +14,7 @@ exports.updateContacts = functions.https.onRequest((request, response) => {
   }
   const promises = [];
   contacts.forEach((uid, i) => {
-    promises.push(db.collection('users').doc(uid).get());
+    promises.push(admin.firestore().collection('users').doc(uid).get());
   })
   Promise.all(promises).then((userResults) => {
     const users = [];
@@ -35,96 +33,31 @@ exports.updateContacts = functions.https.onRequest((request, response) => {
   })
 })
 
-exports.searchUser = functions.https.onRequest((request, response) => {
-  const searchName = request.query.name;
-  const userId = request.query.userid;
-  const friendRequestSentPromise = db.collection(`users/${userId}/friend_request_sent`).get();
-  const friendRequestReceivedPromise = db.collection(`users/${userId}/friend_request_received`).get();
-  const searchUserPromise = db.collection('users/')
-    .get();
-
-  Promise.all([searchUserPromise, friendRequestSentPromise, friendRequestReceivedPromise]).then((results) => {
-    console.log('No of Users', results[0].docs.length)
-
-    const requestSentIds = [];
-    results[1].docs.forEach((doc, index) => {
-      requestSentIds.push(doc.id);
-    })
-    console.log('Request Sent ', requestSentIds);
-
-    const requestReceivedIds = [];
-    results[2].docs.forEach((doc, index) => {
-      requestReceivedIds.push(doc.id);
-    })
-    console.log('Request Received', requestReceivedIds);
-
-    const users = []
-    results[0].docs.forEach((user, index) => {
-      const userData = user.data();
-      if (String(userData.name).toLowerCase().includes(String(searchName).toLowerCase()) && user.id !== userId) {
-        if (requestSentIds.includes(user.id))
-          users.push({ name: userData.name, id: user.id, status: 2 })
-        else if (requestReceivedIds.includes(user.id))
-          users.push({ name: userData.name, id: user.id, status: 3 })
-        else
-          users.push({ name: userData.name, id: user.id, status: 0 })
-      }
-    })
-    response.type('application/json')
-    response.send({ data: users });
-    return;
-  }).catch((r) => {
-    console.log('Catch : ', r)
-  });
-})
-
-exports.sendFriendRequestNotfication = functions.firestore.document("users/{user_id}/friend_request_received/{sent_user_id}").onWrite((event) => {
-  const user_id = event.params.user_id;
-  const sent_user_id = event.params.sent_user_id;
+exports.sendPlayRequestNotfication = functions.firestore.document("users/{user_id}/play_request_received/{sent_user_id}").onWrite((data, context) => {
+  const user_id = context.params.user_id;
+  const sent_user_id = context.params.sent_user_id;
   console.log('Sending notification to ', user_id);
-  if (!event.data.exists) {
-    return console.log('User ', user_id, 'cancelled the friend request');
+  if (!data.after.exists) {
+    console.log('User ', user_id, ' cancelled the friend request');
+    return Promise.resolve();
   }
-  const fcmTokens = db.collection('/users').doc(sent_user_id).get();
-  const sentUserProfile = admin.auth().getUser(sent_user_id);
-  return Promise.all([fcmTokens, sentUserProfile]).then((results) => {
-    const fcmTokens = results[0];
-    const sentUserProfile = results[1];
-    if (!fcmTokens.exists) {
-      return console.log('There are no notification tokens to send to for', user_id);
-    }
-    const tokens = Object.keys(fcmTokens.data().fcm_tokens);
-    console.log('There are', tokens.length, 'tokens to send notifications to for ', user_id);
-    console.log('Fetched follower profile', sentUserProfile);
+
+  return admin.auth().getUserByPhoneNumber(sent_user_id).then((sentUserProfile) => {
+    console.log('Fetched user profile', { id: sentUserProfile.phoneNumber, name: sentUserProfile.name });
 
     const payload = {
-      notification: {
-        title: 'You have a new Friend Request!',
-        body: `${sentUserProfile.displayName} is now following you.`,
-        icon: `${sentUserProfile.photoURL}`
-      },
       data: {
-        title: 'You have a new Friend Request!',
-        action: 'friend_request',
-        body: `${sentUserProfile.displayName} is now following you.`,
-        icon: `${sentUserProfile.photoURL}`
+        title: 'You have a new Play Request!',
+        body: `${sentUserProfile.displayName} sent you Play Request`,
+        userid: sent_user_id,
+        name: `${sentUserProfile.displayName}`,
+        photoUrl: `${sentUserProfile.photoURL}`,
+        action: 'play_request',
       }
     };
 
-    return admin.messaging().sendToDevice(tokens, payload);
-  }).then((response) => {
-    // For each message check if there was an error.
-    const tokensToRemove = [];
-    response.results.forEach((result, index) => {
-      const error = result.error;
-      if (error) {
-        console.error('Failure sending notification to', fcmTokens.data().fcm_tokens[index], error);
-        // Cleanup the tokens who are not registered anymore.
-        if (error.code === 'messaging/invalid-registration-token' || error.code === 'messaging/registration-token-not-registered') {
-          tokensToRemove.push(fcmTokens.ref.child(tokens[index]).remove());
-        }
-      }
-    });
-    return Promise.all(tokensToRemove);
+    return admin.messaging().sendToTopic(user_id.replace('+', '%'), payload);
+  }).catch(e => {
+    console.log(e);
   });
 });
